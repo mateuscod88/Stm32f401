0dfc5edb05c982a922b48867342bf391682f0d32
SMTSOFTWARE\mateusz.malinowski <mateusz.malinowski@intive.com>
2017-04-10 15:33:50 +0200

    HAL UART  interrupts added

--- Gpio_Config.c
+++ Gpio_Config.c
@@ -23,7 +23,49 @@ void gpio_for_button_config()
 	NVIC_EnableIRQ(EXTI0_IRQn);
 	
 }
-
+void gpio_for_uart1TX_config()
+{
+	__HAL_RCC_GPIOB_CLK_ENABLE();
+	GPIO_InitTypeDef gpio_init;
+	gpio_init.Pin |= GPIO_PIN_6 ;
+	gpio_init.Mode = GPIO_MODE_AF_PP ;
+	gpio_init.Pull = GPIO_PULLUP;
+	gpio_init.Speed = GPIO_SPEED_FREQ_HIGH;
+	
+	HAL_GPIO_Init(GPIOB,&gpio_init);
+}
+void gpio_for_uart1RX_config()
+{
+	__HAL_RCC_GPIOB_CLK_ENABLE();
+	GPIO_InitTypeDef gpio_init;
+	gpio_init.Pin |= GPIO_PIN_7 ;
+	gpio_init.Mode = GPIO_MODE_INPUT ;
+	gpio_init.Pull = GPIO_NOPULL;
+	gpio_init.Speed = GPIO_SPEED_FREQ_HIGH;
+	
+	HAL_GPIO_Init(GPIOB,&gpio_init);
+}
+void nvic_for_uart1()
+{
+	NVIC_SetPriority(USART1_IRQn,0);
+	NVIC_EnableIRQ(USART1_IRQn);
+}
+void sendUart()
+{
+	HAL_UART_Transmit_IT();
+}
+void  HAL_UART_TxCpltCallback()
+{
+	tu sie wykonuje po wyslaniu
+}
+void HAL_UART_IRQHandler() 
+{
+	
+}
+void USART1_IRQHandler()
+{
+	
+}
 void EXTI0_IRQHandler()
 	{
 		//HAL_GPIO_WritePin(GPIOD,GPIO_PIN_12,GPIO_PIN_SET);
@@ -32,6 +74,8 @@ void EXTI0_IRQHandler()
 
 void usart_config()
 {
+	
+	
 	UART_InitTypeDef uart_init;
 	uart_init.BaudRate = 9600;
 	uart_init.WordLength = UART_WORDLENGTH_8B;
@@ -39,4 +83,8 @@ void usart_config()
 	uart_init.Parity = UART_PARITY_NONE;
 	uart_init.Mode = UART_MODE_TX_RX;
 	uart_init.OverSampling = UART_OVERSAMPLING_16; 
+	
+	UART_HandleTypeDef huart;
+	huart.Instance = &uart_init;
+	
 }
